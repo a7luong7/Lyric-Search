31af3073609961b664638a1a7e433466fc75161d
a7luong7 <samcheung992@gmail.com>
2021-09-25 22:40:33 -0700

    added skeleton of lyric search with dummy api results

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,68 @@
+import React, { useState } from 'react';
 import './App.css';
+import { searchLyrics } from './musix-match';
 
-const App = () => (
-  <div className="App">
-    asdasdasd
-  </div>
-);
+const SearchForm = ({ setSongResults } : { setSongResults:any }) => {
+  const [searchText, setSearchText] = useState('');
+  const handleSubmit = async (e:any) => {
+    e.preventDefault();
+    if (!searchText) { return; }
+
+    console.log(searchText);
+
+    const lyricsSearchRes = searchLyrics(searchText);
+    console.log('lyrics result', lyricsSearchRes);
+    setSongResults(lyricsSearchRes.track_list);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={searchText}
+        onChange={(e) => setSearchText(e.target.value)}
+      />
+      <button
+        disabled={!searchText}
+        type="submit"
+      >
+        Search
+      </button>
+    </form>
+  );
+};
+
+const SearchResults = ({ songResults } : { songResults:any }) => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const placeholders = [];
+  if (songResults.length === 0) { return (<div>No songs found</div>); }
+
+  return (
+    <div>
+      {songResults.map(({ track } : any) => (
+        <div key={track.track_id} style={{ marginBottom: '.5rem' }}>
+          <div>{track.track_name}</div>
+          <div>
+            {track.artist_name}
+            {' '}
+            -
+            {' '}
+            {track.album_name}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const App = () => {
+  const [songResults, setSongResults] = useState([]);
+  return (
+    <div className="App">
+      <SearchForm setSongResults={setSongResults} />
+      <SearchResults songResults={songResults} />
+    </div>
+  );
+};
 
 export default App;
